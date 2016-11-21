8e7d7fcb07b11cb28caf3638527317af6ace2511
tom_a <tom_a@TomsLaptop>
2016-11-21 12:57:41 +0100

    initial commit

---
+++ Calculator/src/businessObjects/CalculatorStorageBO.java
@@ -0,0 +1,83 @@
+package businessObjects;
+
+public class CalculatorStorageBO {
+
+	private double firstValue;
+	private double secondValue;
+	private double thirdValue;
+	private char operator;
+	private double firstResult;
+	private double secondResult;
+	/**
+	 * @return the firstValue
+	 */
+	public double getFirstValue() {
+		return firstValue;
+	}
+	/**
+	 * @param firstValue the firstValue to set
+	 */
+	public void setFirstValue(double firstValue) {
+		this.firstValue = firstValue;
+	}
+	/**
+	 * @return the secondValue
+	 */
+	public double getSecondValue() {
+		return secondValue;
+	}
+	/**
+	 * @param secondValue the secondValue to set
+	 */
+	public void setSecondValue(double secondValue) {
+		this.secondValue = secondValue;
+	}
+	/**
+	 * @return the thirdValue
+	 */
+	public double getThirdValue() {
+		return thirdValue;
+	}
+	/**
+	 * @param thirdValue the thirdValue to set
+	 */
+	public void setThirdValue(double thirdValue) {
+		this.thirdValue = thirdValue;
+	}
+	/**
+	 * @return the operator
+	 */
+	public char getOperator() {
+		return operator;
+	}
+	/**
+	 * @param operator the operator to set
+	 */
+	public void setOperator(char operator) {
+		this.operator = operator;
+	}
+	/**
+	 * @return the firstResult
+	 */
+	public double getFirstResult() {
+		return firstResult;
+	}
+	/**
+	 * @param firstResult the firstResult to set
+	 */
+	public void setFirstResult(double firstResult) {
+		this.firstResult = firstResult;
+	}
+	/**
+	 * @return the secondResult
+	 */
+	public double getSecondResult() {
+		return secondResult;
+	}
+	/**
+	 * @param secondResult the secondResult to set
+	 */
+	public void setSecondResult(double secondResult) {
+		this.secondResult = secondResult;
+	}
+}
